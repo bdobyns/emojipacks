3a86157d5580e072b82f1347ff51c2f2decc1ce8
Barry A Dobyns <barry@productops.com>
2017-05-09 06:31:19 -0700

    add update=twitch.sh script to this branch

---
+++ update=twitch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+YAML=packs/twitch.yaml
+
+for IMAGE in $( cd assets ; ls )
+do 
+    if ! grep $IMAGE packs/twitch.yaml >/dev/null; then 
+	NAME=$( echo $IMAGE | cut -f 1 -d . ) 
+	NAMES="$NAMES $NAME"
+	(
+	set -x
+	echo "  - name: $NAME" >>$YAML
+	echo "    src: https://github.com/bdobyns/emojipacks/raw/twitch/assets/$IMAGE" >>$YAML
+	)
+    fi
+done
+
+git add $0 assets packs
+git commit -m "added these twitch $NAMES"
+git push
+
+

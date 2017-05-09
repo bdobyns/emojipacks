0677365b383ddb2a73e67ea440db092bb817f594
Barry A Dobyns <barry@productops.com>
2017-05-09 09:08:43 -0700

    add update-pokemongo.sh script to this branch

---
+++ update-pokemongo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+YAML=packs/pokemongo.yaml
+
+for IMAGE in $( cd assets ; ls )
+do 
+    if ! grep $IMAGE packs/pokemongo.yaml >/dev/null; then 
+	NAME=$( echo $IMAGE | cut -f 1 -d . ) 
+	NAMES="$NAMES $NAME"
+	(
+	set -x
+	echo "  - name: $NAME" >>$YAML
+	echo "    src: https://github.com/bdobyns/emojipacks/raw/pokemongo/assets/$IMAGE" >>$YAML
+	)
+    fi
+done
+
+git add $0 assets packs
+git commit -m "added these pokemongo $NAMES"
+git push
+
+
